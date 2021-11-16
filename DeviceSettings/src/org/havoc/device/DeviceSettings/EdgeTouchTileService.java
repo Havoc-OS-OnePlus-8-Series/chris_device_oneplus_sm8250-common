jef00/device_oneplus_sm8250-common_wip

sm8250-common: DeviceSettings: Add unlimit edge touch switch
* /proc/touchpanel/tpedge_limit_enable
  1 = No limit   0 = limit edge touch

  Apparently OOS writes to this node in game mode.

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

DeviceSettings/src/org/aosp/device/DeviceSettings/EdgeTouchTileService.java
@@ -1,0 +1,55 @@
+package org.aosp.device.DeviceSettings;
+
+import android.annotation.TargetApi;
+import android.content.SharedPreferences;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+import org.aosp.device.DeviceSettings.DeviceSettings;
+
+@TargetApi(24)
+public class EdgeTouchTileService extends TileService {
+    private boolean enabled = false;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = EdgeTouchSwitch.isCurrentlyEnabled(this);
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = EdgeTouchSwitch.isCurrentlyEnabled(this);
+        Utils.writeValue(EdgeTouchSwitch.getFile(), enabled ? "0" : "1");
+        sharedPrefs.edit().putBoolean(DeviceSettings.KEY_EDGE_TOUCH, enabled ? false : true).commit();
+        getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+        getQsTile().updateTile();
+    }
+}

DeviceSettings/src/org/aosp/device/DeviceSettings/EdgeTouchSwitch.java
@@ -1,0 +1,35 @@
+package org.aosp.device.DeviceSettings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import org.aosp.device.DeviceSettings.DeviceSettings;
+
+public class EdgeTouchSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/proc/touchpanel/tpedge_limit_enable";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Utils.getFileValueAsBoolean(getFile(), true);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

Havoc-OS-OnePlus-8-Series/chris_device_oneplus_sm8250-common

DeviceSettings/src/org/havoc/device/DeviceSettings/EdgeTouchTileService.java
@@ -1,0 +1,55 @@
+package org.havoc.device.DeviceSettings;
+
+import android.annotation.TargetApi;
+import android.content.SharedPreferences;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+import org.havoc.device.DeviceSettings.DeviceSettings;
+
+@TargetApi(24)
+public class EdgeTouchTileService extends TileService {
+    private boolean enabled = false;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = EdgeTouchSwitch.isCurrentlyEnabled(this);
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = EdgeTouchSwitch.isCurrentlyEnabled(this);
+        Utils.writeValue(EdgeTouchSwitch.getFile(), enabled ? "0" : "1");
+        sharedPrefs.edit().putBoolean(DeviceSettings.KEY_EDGE_TOUCH, enabled ? false : true).commit();
+        getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+        getQsTile().updateTile();
+    }
+}

DeviceSettings/src/org/havoc/device/DeviceSettings/ModeSwitch/EdgeTouchSwitch.java
@@ -1,0 +1,33 @@
+package org.havoc.device.DeviceSettings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+public class EdgeTouchSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/proc/touchpanel/tpedge_limit_enable";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Utils.getFileValueAsBoolean(getFile(), true);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}